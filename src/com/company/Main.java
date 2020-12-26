aec88e7a62dae2f905f591adeb27ed07d7791fbe
Efe UZEL <efeuzel@gmail.com>
2020-12-26 18:27:23 +0300

    Adding selection sort and insertion sort

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,20 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-        int[] myArray = new int[] {5, 4, 3, 2, 1};
-
+        int[] myArray = {5, 4, 3, 2, 1};
         Sorting.BubbleSort(myArray);
+        System.out.println(Arrays.toString(myArray));
+
+        myArray = new int[] {7, 6, 5, 4, 3, 2, 1};
+        Sorting.SelectionSort(myArray);
+        System.out.println(Arrays.toString(myArray));
+
+        myArray = new int[] {7, 2, 5, 4, 3, 2, 1};
+        Sorting.InsertionSort(myArray);
+        System.out.println(Arrays.toString(myArray));
     }
 }
--- src/com/company/Sorting.java
+++ src/com/company/Sorting.java
@@ -1,19 +1,43 @@
 package com.company;
 
-import java.util.Arrays;
-
 public class Sorting {
-    public static void BubbleSort(int[] input) {
-        for(int i = 0; i < input.length -1; i++) {
-            for(int j = 0; j < input.length - 1 - i; j++) {
-                if (input[j] > input[j+1]) {
+    public static void BubbleSort(int[] input) { //0(n^2)
+        for (int i = 0; i < input.length - 1; i++) {
+            for (int j = 0; j < input.length - 1 - i; j++) {
+                if (input[j] > input[j + 1]) {
                     int temp;
-                    temp = input[j+1];
-                    input[j+1] = input[j];
+                    temp = input[j + 1];
+                    input[j + 1] = input[j];
                     input[j] = temp;
                 }
             }
         }
-        System.out.println(Arrays.toString(input));
+    }
+
+    public static void SelectionSort(int[] input) { // O(n^2)
+        for (int i = 0; i < input.length - 2; i++) {
+            int smallestIndex = i;
+            for (int j = i + 1; j < input.length; j++) {
+                if (input[j] < input[smallestIndex]) {
+                    smallestIndex = j;
+                }
+            }
+            int tmp = input[smallestIndex];
+            input[smallestIndex] = input[i];
+            input[i] = tmp;
+        }
+    }
+
+    public static void InsertionSort(int[] input) { //O(n^2), good for
+        //small lists and efficient for nearly sorted lists.
+        for (int i = 1; i < input.length; i++) {
+            int tmp = input[i];
+            int j = i - 1;
+            while (j >= 0 && input[j] > tmp) {
+                input[j + 1] = input[j];
+                j--;
+            }
+            input[j + 1] = tmp;
+        }
     }
 }
